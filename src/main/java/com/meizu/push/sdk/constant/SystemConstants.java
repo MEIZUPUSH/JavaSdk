092a97e6cc733d61698f55c86a933ed5f3789a6f
JasperXgwang <wangxinguo@meizu.com>
2016-12-18 19:43:03 +0800

    修改groupId，发布到sonatype

--- src/main/java/com/meizu/push/sdk/constant/SystemConstants.java
+++ src/main/java/com/meizu/push/sdk/constant/SystemConstants.java
@@ -7,7 +7,7 @@ public class SystemConstants {
 
     public static final String CHAR_SET = "UTF-8";
 
-    public static final String SDK_VERSION = "1.0.0-RC06";
+    public static final String SDK_VERSION = "1.0.0.20171218_release";
 
     /**
      * pushId推送接口（透传消息）
